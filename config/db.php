126cb5970d3f2172d108ec10f497f9c054ae0e72
a.papazyan <a.papazyan@wilix.org>
2019-12-04 13:48:56 +0300

    fix docker-compose

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=localhost;dbname=telegrams',
+    'username' => 'user',
+    'password' => 'password',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
